404d1e5e103522bae03017fb2f89094a58bdc7cf
Bronwyn <Bronwyn@Bronwyn>
2015-10-12 16:44:08 -0400

    First commit and first version of document class

---
+++ src/Document.java
@@ -0,0 +1,35 @@
+//Author: Bronwyn Skelley
+//Version:1 Date: October 12th
+public class Document {
+	private int like;
+	private String name;
+	private String tag;
+	
+	
+	//Contrsuctors
+	public Document(){}
+	
+	public Document(String name, String tag){
+		this.name = name;
+		this.tag = tag;
+	}
+	
+	
+	//accessor methods
+	public String getTag(){
+		return this.tag;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public int getLikes(){
+		return this.like;
+	}
+	
+	//increases like count of document
+	public void likeDoc(){
+		this.like++;
+	}
+}
