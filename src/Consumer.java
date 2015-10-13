291ab9e278bb009feae145de5e1005c1285f6590
Monty1029 <dhanani.monty@gmail.com>
2015-10-13 10:40:21 -0400

    First commit

---
+++ src/Consumer.java
@@ -0,0 +1,8 @@
+/* SYSC3110 Software Design Project
+ * Team redSquadron
+ * Consumer subclass
+ */
+
+public class Consumer extends User {
+
+}
---
+++ src/Producer.java
@@ -0,0 +1,8 @@
+/* SYSC3110 Software Design Project
+ * Team redSquadron
+ * Producer subclass
+ */
+
+public class Producer extends User {
+
+}
