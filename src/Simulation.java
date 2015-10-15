6005bf61d6cebf963ac3608fac21f0787d0b4e72
Evan <EvanBottomley@cmail.carleton.ca>
2015-10-14 21:26:55 -0400

    New method for initializing simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -33,33 +33,49 @@ public class Simulation {
 			map.put(user, l);
 		}
 	}
-
-	public static void main(String[] args) {
-		//Create a new simulation and seed the simulation
-		Simulation sim = new Simulation();
-		sim.allTags.add("Test1");
-		sim.availableTags.add("Test1");
+	
+	public void seed() {
+		allTags.add("Jazz");
+		allTags.add("Techno");
+		allTags.add("Metal");
+		allTags.add("Classical");
+		allTags.add("Rock");
 		//Add users
 		User u;
+		Random rand = new Random();
+		int r;
 		for (int i = 0; i < 5; i++) {
-			u = new Consumer(("Consumer"+i), "Test1");
+			r = rand.nextInt(allUser.size());
+			u = new Consumer(("Consumer"+i), allTags.get(r));
 			allUser.add(u);
-			u = new Producer("Producer"+i, "Test1");
+			r = rand.nextInt(allUser.size());
+			u = new Producer(("Producer"+i), allTags.get(r));
 			allUser.add(u);
 		}
 		//Add documents
 		Document d;
 		for (int i = 0; i < 5; i++) {
-			d = new Document(("Doc" + i), "Test1");
+			r = rand.nextInt(allUser.size());
+			d = new Document(("Doc" + i), allTags.get(r));
 			allDoc.add(d);
 		}
-		
+	}
+
+	public static void main(String[] args) {
+		//Create a new simulation and seed the simulation
+		Simulation sim = new Simulation(5);
+		sim.seed();
 		Random rand = new Random();
 		int x;
 		int loops = 0;
+		List<Document> liked;
 		while(loops < 100) {
-			x = rand.nextInt(allUser.size()) + 1;
-			allUser.get(x).act();
+			x = rand.nextInt(sim.allUser.size());
+			User acting = sim.allUser.get(x);
+			liked = acting.act(sim.allDoc);
+			for(Document doc: liked) {
+				sim.addLike(acting, doc);
+			}
 			loops++;
 		}
 		System.out.println("End simulation.");
