8a7aadec163f56a01a71a84dae5d079bc2e074f2
Bronwyn <Bronwyn@Bronwyn>
2015-10-16 10:38:15 -0400

    Added javadoc comments

--- src/Document.java
+++ src/Document.java
@@ -6,28 +6,45 @@ public class Document {
 	private String tag;
 	
 	
-	//Contrsuctor
-	
+	/**
+	 * Constructor using name and tag to make document
+	 * @param name this is name of document
+	 * @param tag this is tag of document
+	 */
 	public Document(String name, String tag){
 		this.name = name;
 		this.tag = tag;
 	}
 	
 	
-	//accessor methods
+	/**
+	 * gets the tag of document
+	 * @return tag of document 
+	 */
 	public String getTag(){
 		return this.tag;
 	}
 	
+	/**
+	 * gets name of document
+	 * @return name of document
+	 */
 	public String getName(){
 		return this.name;
 	}
 	
+	/**
+	 * gets how many times the document has been liked
+	 * @return how many times the document has been liked
+	 */
 	public int getLikes(){
 		return this.like;
 	}
 	
-	//increases like count of document
+	
+	/**
+	 * increments by one the likes of the document
+	 */
 	public void likeDoc(){
 		this.like++;
 	}
