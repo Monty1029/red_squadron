7b8737248cc50d78ab9aa2c5ddc564c44342ffd6
Monty1029 <dhanani.monty@gmail.com>
2015-10-16 10:39:41 -0400

    Added documentation

--- src/Consumer.java
+++ src/Consumer.java
@@ -7,11 +7,20 @@
 import java.util.*;
 
 public class Consumer extends User {
-
+	/**
+	 * Creates a Consumer object
+	 * @param username of the Consumer
+	 * @param taste that the Consumer likes the most
+	 * @param sim reference to the Simulation class
+	 */
 	public Consumer(String username, String taste, Simulation sim) {
 		super(username, taste, sim);
 	}
 
+	/**
+	 * Cycles through all the existing documents and returns an ArrayList
+	 * of Documents with the same taste as the Consumer
+	 */
 	public ArrayList<Document> act(ArrayList<Document> d) {
 		ArrayList<Document> sameTagDocs = new ArrayList<Document>();
 
@@ -23,6 +32,11 @@ public class Consumer extends User {
 		return sameTagDocs;
 	}
 
+	/**
+	 * Ranks all of the documents and returns the top 10
+	 * @param unrankedDocs is the list of unranked documents to rank
+	 * @return a list of ranked documents
+	 */
 	public ArrayList<Document> popularityRank(List<Document> unrankedDocs) {
 		ArrayList<Document> ranked = new ArrayList<Document>();
 		Collections.sort(unrankedDocs.getLikes());
@@ -38,6 +52,17 @@ public class Consumer extends User {
 	 * milestone 2 }
 	 */
 
+	/**
+	 * Cycles through the list of ranked documents and every time it sees
+	 * a document that matches the Producer's taste, and if the Producer hasn't liked it yet,
+	 * increment a counter by 1.
+	 * @param docs is a list of ranked documents
+	 * @return the number of documents that appear in the ranked list that
+	 * 			match the Producer's taste
+	 */
+	/*NOTE: For now, payoff method in both Consumer and Producer class are copy & pasted code,
+	but they will be different in Milestone 2
+	*/
 	public int payoff(ArrayList<Document> d) {
 		int pointCounter = 0;
 
--- src/Producer.java
+++ src/Producer.java
@@ -15,14 +15,12 @@ public class Producer extends User {
 		produced = new ArrayList<Document>();
 	}
 
+	/**
+	 * Cycles through all the existing documents and returns an ArrayList
+	 * of Documents with the same taste as the Consumer.
+	 * Producer also likes every Document it creates
+	 */
 	public ArrayList<Document> act(ArrayList<Document> allDocs) {
-		/*
-		 * goes through d d is every single document create new list sameTaste
-		 * for every document in the d list, add all the matching tastes to
-		 * sameTaste while liking that document return list of liked docs
-		 * 
-		 */
-
 		ArrayList<Document> sameTagDocs = new ArrayList<Document>();
 
 		for (Document d : allDoc) {
@@ -33,12 +31,20 @@ public class Producer extends User {
 		}
 		return sameTagDocs;
 	}
-
+	/**
+	 * Creates a new Document of the Producer's favourite taste and adds
+	 * the document to an ArrayList of documents that it has produced.
+	 */
 	public void produce() {
 		Document newlyProduced = new Document("" + super.getName() + super.produced.getSize(), super.getTaste());
 		produced.add(newlyProduced);
 	}
 
+	/**
+	 * Ranks all of the documents and returns the top 10
+	 * @param unrankedDocs is the list of unranked documents
+	 * @return a list of ranked documents
+	 */
 	public ArrayList<Document> rank(List<Document> unrankedDocs) {
 		ArrayList<Document> ranked = new ArrayList<Document>();
 		Collections.sort(unrankedDocs.getLikes());
@@ -49,14 +55,17 @@ public class Producer extends User {
 		return ranked;
 	}
 
+	/**
+	 * Cycles through the list of ranked documents and every time it sees
+	 * a document that matches the Producer's taste, and if the Producer hasn't liked it yet,
+	 * increment a counter by 1.
+	 * @param docs is a list of ranked documents
+	 * @return the number of documents that appear in the ranked list that
+	 * 			match the Producer's taste
+	 */
 	public int payoff(ArrayList<Document> docs) {
-		/*
-		 * cycle through the list of ranked documents and everytime it sees a document that matches
-		 * 						my taste and i haven't liked it, increment a counter by 1
-		 * 	return counter value
-		 */
 		
-		int pointCounter = 0;
+		int pointCounter = 0; //Keeps track of how many documents appear that match the Producer's taste
 		
 		for (Document d : docs) {
 			if ((d.getTag().equals(super.getTaste())) && (super.hasLiked())) {
