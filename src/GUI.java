0c7aa3887cda7fcce54a661e14f05bddfa8be27c
Monty1029 <dhanani.monty@gmail.com>
2015-10-31 10:25:25 -0400

    First commit of milestone 2.

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+/* SYSC3110 Software Design Project
+ * Team redSquadron
+ * GUI class by Monty Dhanani
+ */
+public class GUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
