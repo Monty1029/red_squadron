1c54beffdf06de23ddb512070ff051ceaf87a74a
Bronwyn <Bronwyn@Bronwyn>
2015-11-06 12:10:53 -0500

    Added document testing and start of simulation testing

--- src/DocumentTest.java
+++ src/DocumentTest.java
@@ -7,42 +7,28 @@ public class DocumentTest {
 
 	@Test
 	public void testDocument() {
-		fail("Not yet implemented");
+		Document doc = new Document("name", "tag");
+		assertTrue(doc.getName().equals("name")&&doc.getTag().equals("tag"));
 	}
 
-	@Test
-	public void testGetTag() {
-		fail("Not yet implemented");
-	}
 
 	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
+	public void testGetLikeData() {
+		Document doc = new Document("name", "tag");
+		Simulation sim = new Simulation(null);
+		Consumer con = new Consumer("user", "tag", sim);
+		doc.likeDoc(con);
+		assertTrue(doc.hasLiked(con));
+		assertTrue(doc.getLikes()==1);
+		assertTrue(doc.getLikedUsers().contains(con));
 	}
 
-	@Test
-	public void testGetLikes() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testLikeDoc() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testHasLiked() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetLikedUsers() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testCompareTo() {
-		fail("Not yet implemented");
+		Document doc = new Document("name", "tag");
+		Document doc1 = new Document("name", "tag");
+		assertTrue(doc.compareTo(doc1)==0);
 	}
 
 }
---
+++ src/SimulationTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SimulationTest {
+
+	@Test
+	public void testSelectTags() {
+		Simulation sim = new Simulation(null);
+		sim.selectTags(1);
+		sim.getAllTags();
+	}
+
+	@Test
+	public void testAddLike() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResults() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStep() {
+		fail("Not yet implemented");
+	}
+
+}
